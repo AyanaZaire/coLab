e9fc0a8e7471a1b20d8299f79268bec48a1968c0
finale100 <finale150@gmail.com>
2018-09-10 18:15:22 -0400

    built projects controller

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::ProjectsController < ApplicationController
+
+  before_action :find_project, only: [:show]
+  def index
+    @projects = Project.all
+    render json: @projects
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    render json: Project.create(project_params)
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.permit(:title, :img_url, :description, :start_date, :end_date, :max_member)
+  end
+
 end
