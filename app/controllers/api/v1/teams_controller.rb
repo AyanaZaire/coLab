ca47d4fbff0a5e0ff70fd181a7cffab3012d1d93
finale100 <finale150@gmail.com>
2018-09-11 18:29:14 -0400

    created team controller action for create

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::TeamsController < ApplicationController
+  def create
+    render json: Team.create(team_params)
+  end
+
+  private
+
+  def team_params
+    params.permit(:user_id, :project_id)
+  end
+
 end
