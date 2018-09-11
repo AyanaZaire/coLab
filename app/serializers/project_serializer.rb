1ee6e662a8ce512f02c4b5096c96589c18d330b1
finale100 <finale150@gmail.com>
2018-09-11 11:04:41 -0400

    update project controller

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :title, :img_url, :description, :start_date, :end_date, :max_member, :users, :teams
+  attributes :id, :title, :img_url, :description, :start_date, :end_date, :max_member, :users, :teams
   has_many :teams
   has_many :users, through: :teams
 end
